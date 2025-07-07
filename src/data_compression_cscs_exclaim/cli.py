01cf40840ef0886618be277d95a3f5be252d0cf5
Nicoletta Farabullini <41536517+nfarabullini@users.noreply.github.com>
2025-07-07 17:08:17 +0200

    clutsering interactive plots

--- src/data_compression_cscs_exclaim/cli.py
+++ src/data_compression_cscs_exclaim/cli.py
@@ -28,6 +28,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import StandardScaler
+import plotly.io as pio
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 import warnings
 warnings.filterwarnings(
@@ -154,6 +159,10 @@ def summarize_compression(netcdf_file: str, field_to_compress: str):
     ds = utils.open_netcdf(netcdf_file, field_to_compress)
     da = ds[field_to_compress]
 
+    #extract 20% of dataset
+    # slices = {dim: slice(0, int(size * 0.8)) for dim, size in da.sizes.items()}
+    # da = da.isel(**slices)
+
     compressors = utils.compressor_space(da)
     filters = utils.filter_space(da)
     serializers = utils.serializer_space(da)
@@ -164,14 +173,15 @@ def summarize_compression(netcdf_file: str, field_to_compress: str):
     num_loops = num_compressors * num_filters * num_serializers
     click.echo(f"Number of loops: {num_loops} ({num_compressors} compressors, {num_filters} filters, {num_serializers} serializers)")
 
-    # Lookup table for valid theresholds
+    # Lookup table for valid thresholds
     # https://docs.google.com/spreadsheets/d/1lHcX-HE2WpVCOeKyDvM4iFqjlWvkd14lJlA-CUoCxMM/edit?gid=0#gid=0
     sheet_id = "1lHcX-HE2WpVCOeKyDvM4iFqjlWvkd14lJlA-CUoCxMM"
     sheet_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
-    thersholds = pd.read_csv(sheet_url)
+    thresholds = pd.read_csv(sheet_url)
 
     results = []
     raw_values_explicit = []
+    raw_values_explicit_with_names = []
     for compressor, filter, serializer in tqdm(
         itertools.product(compressors, filters, serializers),
         total=num_loops,
@@ -191,13 +201,14 @@ def summarize_compression(netcdf_file: str, field_to_compress: str):
         l2_error_rel = errors["Relative_Error_L2"]
         linf_error_rel = errors["Relative_Error_Linf"]
         raw_values_explicit.append((compression_ratio, l1_error_rel, l2_error_rel, linf_error_rel, dwt_dist))
-
         # TODO: refine criteria based on the thersholds table
         if l1_error_rel <= 1e-2:
             results.append(((compressor, filter, serializer), compression_ratio, l1_error_rel, dwt_dist))
+            raw_values_explicit_with_names.append((compression_ratio, l1_error_rel, l2_error_rel, linf_error_rel, dwt_dist, str(compressor.codec_name), str(filter.codec_name), str(serializer.codec_name)))
     
     # Needed for clustering
     np.save('scored_results_raw.npy', np.asarray(pd.DataFrame(raw_values_explicit)))
+    np.save('scored_results_with_names.npy', np.asarray(pd.DataFrame(raw_values_explicit_with_names)))
 
     max_compression_ratio = 0
     for (cfg, ratio, error, dwt) in results:
@@ -208,121 +219,210 @@ def summarize_compression(netcdf_file: str, field_to_compress: str):
     click.echo("Best combo (valid threshold & max CR):")
     click.echo(f"\ {best_combo[0]} \ --> Ratio: {best_combo[1]:.3f} | Error: {best_combo[2]:.3e} | DWT: {best_combo[3]:.3e}")
 
-
 @cli.command("perform_clustering")
-@click.argument("field_to_compress")
-def perform_clustering(field_to_compress: str):
-    scored_results = np.load('scored_results_raw.npy', allow_pickle=True)
+@click.argument("npy_file", type=click.Path(exists=True, dir_okay=False))
+def perform_clustering(npy_file: str):
+    scored_results = np.load(npy_file, allow_pickle=True)
 
     scored_results_pd = pd.DataFrame(scored_results)
 
-    scored_results_np_l1 = np.hstack((np.asarray(scored_results_pd[[0]]), np.asarray(scored_results_pd[[1]])))
-    scored_results_np_l2 = np.hstack((np.asarray(scored_results_pd[[0]]), np.asarray(scored_results_pd[[2]])))
-    scored_results_np_linf = np.hstack((np.asarray(scored_results_pd[[0]]), np.asarray(scored_results_pd[[3]])))
-    scored_results_np_dwt = np.hstack((np.asarray(scored_results_pd[[0]]), np.asarray(scored_results_pd[[4]])))
+    numeric_cols = scored_results_pd.select_dtypes(include=[np.number]).columns
+    mask = np.isfinite(scored_results_pd[numeric_cols]).all(axis=1)
+    scored_results_pd = scored_results_pd[mask].dropna()
+    clean_arr_dwt = np.hstack((np.asarray(scored_results_pd[[0]]), np.asarray(scored_results_pd[[4]])))
 
-    scored_results_np_l1=scored_results_np_l1.astype(float)
-    scored_results_np_l2 = scored_results_np_l2.astype(float)
-    scored_results_np_linf = scored_results_np_linf.astype(float)
-    scored_results_np_dwt = scored_results_np_dwt.astype(float)
+    k_values = range(3, 10)
+    inertias = []
+    silhouette_scores = []
 
-    clean_arr_l1 = scored_results_np_l1[~np.isnan(scored_results_np_l1).any(axis=1)]
-    clean_arr_l1 = clean_arr_l1[~np.isinf(clean_arr_l1).any(axis=1)]
-    clean_arr_l2 = scored_results_np_l2[~np.isnan(scored_results_np_l2).any(axis=1)]
-    clean_arr_l2 = clean_arr_l2[~np.isinf(clean_arr_l2).any(axis=1)]
-    clean_arr_linf = scored_results_np_linf[~np.isnan(scored_results_np_linf).any(axis=1)]
-    clean_arr_linf = clean_arr_linf[~np.isinf(clean_arr_linf).any(axis=1)]
-    clean_arr_dwt = scored_results_np_dwt[~np.isnan(scored_results_np_dwt).any(axis=1)]
-    clean_arr_dwt = clean_arr_dwt[~np.isinf(clean_arr_dwt).any(axis=1)]
+    for k in tqdm(k_values, desc="Looping over k values"):
+        kmeans = KMeans(n_clusters=k, random_state=0, n_init="auto")
+        labels = kmeans.fit_predict(clean_arr_dwt)
+        inertias.append(kmeans.inertia_)
+        silhouette_scores.append(silhouette_score(clean_arr_dwt, labels))
 
-    # # Range of clusters to try
-    # k_values = range(3, 10)
-    # inertias = []
-    # silhouette_scores = []
-    #
-    # for k in tqdm(k_values, desc="Looping over k values"):
-    #     kmeans = KMeans(n_clusters=k, random_state=0, n_init="auto")
-    #     labels = kmeans.fit_predict(clean_arr_dwt)
-    #     inertias.append(kmeans.inertia_)
-    #     silhouette_scores.append(silhouette_score(clean_arr_dwt, labels))
-    #
-    # # Plot Elbow Curve
-    # plt.figure(figsize=(12, 5))
-    # plt.subplot(1, 2, 1)
-    # plt.plot(k_values, inertias, 'bo-')
-    # plt.xlabel('Number of Clusters (k)')
-    # plt.ylabel('Inertia')
-    # plt.title('Elbow Method for Optimal k')
-    # # Plot Silhouette Score
-    # plt.subplot(1, 2, 2)
-    # plt.plot(k_values, silhouette_scores, 'go-')
-    # plt.xlabel('Number of Clusters (k)')
-    # plt.ylabel('Silhouette Score')
-    # plt.title('Silhouette Score for Optimal k')
-    # plt.tight_layout()
-    # plt.show()
+    # Plot Elbow Curve
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(k_values, inertias, 'bo-')
+    plt.xlabel('Number of Clusters (k)')
+    plt.ylabel('Inertia')
+    plt.title('Elbow Method for Optimal k')
+    # Plot Silhouette Score
+    plt.subplot(1, 2, 2)
+    plt.plot(k_values, silhouette_scores, 'go-')
+    plt.xlabel('Number of Clusters (k)')
+    plt.ylabel('Silhouette Score')
+    plt.title('Silhouette Score for Optimal k')
+    plt.tight_layout()
+    plt.show()
+
+@cli.command("analyze_clustering")
+@click.argument("npy_file", type=click.Path(exists=True, dir_okay=False))
+def analyze_clustering(npy_file: str):
+    scored_results = np.load(str(npy_file), allow_pickle=True)
+
+    scored_results_pd = pd.DataFrame(scored_results)
+
+    numeric_cols = scored_results_pd.select_dtypes(include=[np.number]).columns
+    mask = np.isfinite(scored_results_pd[numeric_cols]).all(axis=1)
+    scored_results_pd = scored_results_pd[mask].dropna()
+
+    clean_arr_l1 = np.hstack((np.asarray(scored_results_pd[[0]]),
+                                      np.asarray(scored_results_pd[[1]]),
+                                      np.asarray(scored_results_pd[[5]]),
+                                      np.asarray(scored_results_pd[[6]]),
+                                      np.asarray(scored_results_pd[[7]]),
+                                      ))
+
+    clean_arr_l2 = np.hstack((np.asarray(scored_results_pd[[0]]),
+                                      np.asarray(scored_results_pd[[2]]),
+                                      np.asarray(scored_results_pd[[5]]),
+                                      np.asarray(scored_results_pd[[6]]),
+                                      np.asarray(scored_results_pd[[7]]),
+                                      ))
+
+    clean_arr_linf = np.hstack((np.asarray(scored_results_pd[[0]]),
+                                      np.asarray(scored_results_pd[[3]]),
+                                      np.asarray(scored_results_pd[[5]]),
+                                      np.asarray(scored_results_pd[[6]]),
+                                      np.asarray(scored_results_pd[[7]]),
+                                      ))
+
+    clean_arr_dwt = np.hstack((np.asarray(scored_results_pd[[0]]),
+                                      np.asarray(scored_results_pd[[4]]),
+                                      np.asarray(scored_results_pd[[5]]),
+                                      np.asarray(scored_results_pd[[6]]),
+                                      np.asarray(scored_results_pd[[7]]),
+                                      ))
+
 
     # Plot Error and Similarity Metrics VS Ratio
     kmeans = KMeans(n_clusters=5, random_state=0, n_init="auto")
-    plt.figure(figsize=(12, 5))
+
+    fig = make_subplots(rows=2, cols=2,
+                        subplot_titles=[
+                            "L1 VS Ratio KMeans Clustering", "L2 VS Ratio KMeans Clustering",
+                            "LInf VS Ratio KMeans Clustering", "DWT VS Ratio KMeans Clustering"
+                        ])
 
     # L1 clustering
-    # Filtering outliers
-    # clean_arr_l1=clean_arr_l1[np.where(clean_arr_l1[:, 1] < 400)] # t
-    clean_arr_l1=clean_arr_l1[np.where(clean_arr_l1[:, 1] < 200)] # q
-    y_kmeans = kmeans.fit_predict(clean_arr_l1)
-    plt.subplot(2, 2, 1)
-    plt.scatter(clean_arr_l1[:, 0], clean_arr_l1[:, 1], c=y_kmeans, cmap='viridis', s=50)
-    plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=200, c='red', marker='X', label='Centroids')
-    plt.title("L1 VS Ratio KMeans Clustering")
-    plt.xlabel("Ratio")
-    plt.ylabel("L1")
-    plt.legend()
-    plt.grid(True)
+    clean_arr_l1_filtered = np.column_stack((clean_arr_l1[:, 0].astype(float), clean_arr_l1[:, 1].astype(float)))
+    y_kmeans = kmeans.fit_predict(clean_arr_l1_filtered)
+    df_l1 = pd.DataFrame(clean_arr_l1_filtered, columns=["Ratio", "L1"])
+    df_l1["cluster"] = y_kmeans.astype(str)
+    df_l1["compressor"] = clean_arr_l1[:, 2]
+    df_l1["filter"] = clean_arr_l1[:, 3]
+    df_l1["serializer"] = clean_arr_l1[:, 4]
+    fig_l1 = px.scatter(df_l1, x="Ratio", y="L1", color="cluster",
+                     title="L1 VS Ratio KMeans Clustering",
+                     hover_data=["compressor", "filter", "serializer"])
+
+    fig.add_trace(
+        go.Scatter(
+            x=kmeans.cluster_centers_[:, 0],
+            y=kmeans.cluster_centers_[:, 1],
+            mode="markers+text",
+            marker=dict(color="black", size=12, symbol="x"),
+            textposition="top center",
+            name="Centroids",
+            showlegend=True
+        ),
+        row=1,
+        col=1
+    )
+    for trace in fig_l1.data:
+        fig.add_trace(trace, row=1, col=1)
 
     # L2 clustering
-    # Filtering outliers
-    # clean_arr_l2 = clean_arr_l2[np.where(clean_arr_l2[:, 1] < 400)] # t
-    clean_arr_l2 = clean_arr_l2[np.where(clean_arr_l2[:, 1] < 150)]  # q
-    y_kmeans = kmeans.fit_predict(clean_arr_l2)
-    plt.subplot(2, 2, 2)
-    plt.scatter(clean_arr_l2[:, 0], clean_arr_l2[:, 1], c=y_kmeans, cmap='viridis', s=50)
-    plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=200, c='red', marker='X', label='Centroids')
-    plt.title("L2 VS Ratio  KMeans Clustering")
-    plt.xlabel("Ratio")
-    plt.ylabel("L2")
-    plt.legend()
-    plt.grid(True)
+    clean_arr_l2_filtered = np.column_stack((clean_arr_l2[:, 0].astype(float), clean_arr_l2[:, 1].astype(float)))
+    y_kmeans = kmeans.fit_predict(clean_arr_l2_filtered)
+    df_l2 = pd.DataFrame(clean_arr_l2_filtered, columns=["Ratio", "L2"])
+    df_l2["cluster"] = y_kmeans.astype(str)
+    df_l2["compressor"] = clean_arr_l2[:, 2]
+    df_l2["filter"] = clean_arr_l2[:, 3]
+    df_l2["serializer"] = clean_arr_l2[:, 4]
+    fig_l2 = px.scatter(df_l2, x="Ratio", y="L2", color="cluster",
+                     title="L2 VS Ratio KMeans Clustering",
+                     hover_data=["compressor", "filter", "serializer"])
 
-    # LIng clustering
-    # Filtering outliers
-    # clean_arr_linf = clean_arr_linf[np.where(clean_arr_linf[:, 1] < 6)] # t
-    clean_arr_linf = clean_arr_linf[np.where(clean_arr_linf[:, 1] < 60)]  # q
-    y_kmeans = kmeans.fit_predict(clean_arr_linf)
-    plt.subplot(2, 2, 3)
-    plt.scatter(clean_arr_linf[:, 0], clean_arr_linf[:, 1], c=y_kmeans, cmap='viridis', s=50)
-    plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=200, c='red', marker='X', label='Centroids')
-    plt.title("LInf VS Ratio KMeans Clustering")
-    plt.xlabel("Ratio")
-    plt.ylabel("LInf")
-    plt.grid(True)
-    plt.legend()
+    fig.add_trace(
+        go.Scatter(
+            x=kmeans.cluster_centers_[:, 0],
+            y=kmeans.cluster_centers_[:, 1],
+            mode="markers+text",
+            marker=dict(color="black", size=12, symbol="x"),
+            textposition="top center",
+            name="Centroids",
+            showlegend=False
+        ),
+        row=1,
+        col=2
+    )
+    for trace in fig_l2.data:
+        fig.add_trace(trace, row=1, col=2)
+
+
+    # LInf clustering
+    clean_arr_linf_filtered = np.column_stack((clean_arr_linf[:, 0].astype(float), clean_arr_linf[:, 1].astype(float)))
+    y_kmeans = kmeans.fit_predict(clean_arr_linf_filtered)
+    df_linf = pd.DataFrame(clean_arr_linf_filtered, columns=["Ratio", "LInf"])
+    df_linf["cluster"] = y_kmeans.astype(str)
+    df_linf["compressor"] = clean_arr_linf[:, 2]
+    df_linf["filter"] = clean_arr_linf[:, 3]
+    df_linf["serializer"] = clean_arr_linf[:, 4]
+    fig_linf = px.scatter(df_linf, x="Ratio", y="LInf", color="cluster",
+                     title="LInf VS Ratio KMeans Clustering",
+                     hover_data=["compressor", "filter", "serializer"])
+
+    fig.add_trace(
+        go.Scatter(
+            x=kmeans.cluster_centers_[:, 0],
+            y=kmeans.cluster_centers_[:, 1],
+            mode="markers+text",
+            marker=dict(color="black", size=12, symbol="x"),
+            textposition="top center",
+            name="Centroids",
+            showlegend=False
+        ),
+        row=2,
+        col=1
+    )
+    for trace in fig_linf.data:
+        fig.add_trace(trace, row=2, col=1)
+
 
     # DWT clustering
-    # Filtering outliers
-    # clean_arr_dwt = clean_arr_dwt[np.where(clean_arr_dwt[:, 1] < 1e4)] # t
-    clean_arr_dwt = clean_arr_dwt[np.where(clean_arr_dwt[:, 1] < 200)]  # q
-    y_kmeans = kmeans.fit_predict(clean_arr_dwt)
-    plt.subplot(2, 2, 4)
-    plt.scatter(clean_arr_dwt[:, 0], clean_arr_dwt[:, 1], c=y_kmeans, cmap='viridis', s=50)
-    plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=200, c='red', marker='X', label='Centroids')
-    plt.title("DWT VS Ratio KMeans Clustering")
-    plt.xlabel("Ratio")
-    plt.ylabel("DWT")
-    plt.legend()
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
+    clean_arr_dwt_filtered = np.column_stack((clean_arr_dwt[:, 0].astype(float), clean_arr_dwt[:, 1].astype(float)))
+    y_kmeans = kmeans.fit_predict(clean_arr_dwt_filtered)
+    df_dwt = pd.DataFrame(clean_arr_dwt_filtered, columns=["Ratio", "DWT"])
+    df_dwt["cluster"] = y_kmeans.astype(str)
+    df_dwt["compressor"] = clean_arr_dwt[:, 2]
+    df_dwt["filter"] = clean_arr_dwt[:, 3]
+    df_dwt["serializer"] = clean_arr_dwt[:, 4]
+    fig_dwt = px.scatter(df_dwt, x="Ratio", y="DWT", color="cluster",
+                     title="DWT VS Ratio KMeans Clustering",
+                     hover_data=["compressor", "filter", "serializer"])
+
+    fig.add_trace(
+        go.Scatter(
+            x=kmeans.cluster_centers_[:, 0],
+            y=kmeans.cluster_centers_[:, 1],
+            mode="markers+text",
+            marker=dict(color="black", size=12, symbol="x"),
+            textposition="top center",
+            name="Centroids",
+            showlegend=False
+        ),
+        row=2,
+        col=2
+    )
+    for trace in fig_dwt.data:
+        fig.add_trace(trace, row=2, col=2)
+
+    fig.update_layout(title="", showlegend=False)
+    pio.renderers.default = "browser"
+    fig.show()
 
 
 @cli.command("models_evaluation")
