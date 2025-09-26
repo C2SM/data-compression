f7adfa6ee5cf830dd6951f5c4ee492d7b3de9662
Mikael Stellio <mikael.stellio@meteoswiss.ch>
2025-09-26 19:49:43 +0200

    Renaming of threshold

--- src/dc_toolkit/cli.py
+++ src/dc_toolkit/cli.py
@@ -780,10 +780,10 @@ def plot_compression_errors(dataset_file: str, where_to_write: str, field_to_com
         click.echo(f"Field {field_to_compress} should be in lat-lon form, i.e. dimensions (lat, lon)! It currently has dimensions: {da.dims}.")
         sys.exit(1)
 
-    threshold = 2.5  # GiB
-    if da_memsize / (1024 ** 3) > threshold:
+    mem_threshold = 2.5  # GiB
+    if da_memsize / (1024 ** 3) > mem_threshold:
         click.echo(f"Field {field_to_compress} is too large ({humanize.naturalsize(da_memsize, binary=True)}). "
-                   f"To avoid high memory usage we only support fields up to {threshold} GiB.")
+                   f"To avoid high memory usage we only support fields up to {mem_threshold} GiB.")
         sys.exit(1)
 
     compressors = utils.compressor_space(da, with_lossy, with_numcodecs_wasm, with_ebcc, compressor_class)
